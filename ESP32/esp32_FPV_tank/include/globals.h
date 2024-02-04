9c97a809ce5c6550ef72f189384495b3618c6b31
Grant Croft <grantcroft.25@gmail.com>
2024-02-04 17:11:47 -0500

    adding dc motor obj

---
+++ ESP32/esp32_FPV_tank/include/globals.h
@@ -0,0 +1,8 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include "MotorController_DRV8833.h"
+
+extern MotorController_DRV8833 motorController;  // Create Motor Controller Object
+
+#endif // GLOBALS_H
---
+++ ESP32/esp32_FPV_tank/src/globals.cpp
@@ -0,0 +1,4 @@
+#include "globals.h"
+
+
+MotorController_DRV8833 motorController;  // Create Motor Controller Object
